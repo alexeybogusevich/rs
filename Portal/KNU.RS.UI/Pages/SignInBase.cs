aed6bf59c7d5abd71ee5519b421e6c949a09fc99
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-11 16:19:07 +0300

    blazor nick chapsas

---
+++ Portal/KNU.RS.UI/Pages/SignInBase.cs
@@ -0,0 +1,31 @@
+﻿using KNU.RS.Logic.Models.Account;
+using KNU.RS.Logic.Services.AccountService;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace KNU.RS.UI.Pages
+{
+    public class SignInBase : ComponentBase
+    {
+        [Inject]
+        protected IAccountService AccountService { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected LoginModel LoginModel { get; set; } = new LoginModel { Email = string.Empty, Password = string.Empty };
+
+        protected async Task LoginAsync()
+        {
+            var loginResult = await AccountService.LoginAsync(LoginModel);
+
+            if (loginResult)
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+
+
+        }
+    }
+}
