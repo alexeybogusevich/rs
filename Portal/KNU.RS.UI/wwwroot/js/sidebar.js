35a2f6cd68f60b73baf73231ff9a5e8490b7df45
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-21 12:53:26 +0300

    added pre sidebar activity

---
+++ Portal/KNU.RS.UI/wwwroot/js/sidebar.js
@@ -0,0 +1,58 @@
+﻿window.blazorSidebarExtensions = {
+    SET_PANEL_ACTIVE: function setAuthNFailed() {
+        if (!($('#side-li-panel').hasClass('active'))) {
+            $('#side-li-panel').addClass('active');
+        }
+        if ($('#side-li-doctors').hasClass('active')) {
+            $('#side-li-doctors').removeClass('active');
+        }
+        if ($('#side-li-patients').hasClass('active')) {
+            $('#side-li-patients').removeClass('active');
+        }
+        if ($('#side-li-settings').hasClass('active')) {
+            $('#side-li-settings').removeClass('active');
+        }
+    },
+    SET_DOCTORS_ACTIVE: function setAuthNFailed() {
+        if ($('#side-li-panel').hasClass('active')) {
+            $('#side-li-panel').removeClass('active');
+        }
+        if (!($('#side-li-doctors').hasClass('active'))) {
+            $('#side-li-doctors').addClass('active');
+        }
+        if ($('#side-li-patients').hasClass('active')) {
+            $('#side-li-patients').removeClass('active');
+        }
+        if ($('#side-li-settings').hasClass('active')) {
+            $('#side-li-settings').removeClass('active');
+        }
+    },
+    SET_PATIENTS_ACTIVE: function setAuthNFailed() {
+        if ($('#side-li-panel').hasClass('active')) {
+            $('#side-li-panel').addClass('active');
+        }
+        if ($('#side-li-doctors').hasClass('active')) {
+            $('#side-li-doctors').removeClass('active');
+        }
+        if (!($('#side-li-patients').hasClass('active'))) {
+            $('#side-li-patients').addClass('active');
+        }
+        if ($('#side-li-settings').hasClass('active')) {
+            $('#side-li-settings').removeClass('active');
+        }
+    },
+    SET_SETTINGS_ACTIVE: function setAuthNFailed() {
+        if ($('#side-li-panel').hasClass('active')) {
+            $('#side-li-panel').removeClass('active');
+        }
+        if ($('#side-li-doctors').hasClass('active')) {
+            $('#side-li-doctors').removeClass('active');
+        }
+        if (!($('#side-li-patients').hasClass('active'))) {
+            $('#side-li-patients').addClass('active');
+        }
+        if ($('#side-li-settings').hasClass('active')) {
+            $('#side-li-settings').removeClass('active');
+        }
+    }
+}
