f26fd28724ff580a19974bdce1f7d8acca05edb2
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-21 13:39:36 +0300

    trying to fix sidebar

--- Portal/KNU.RS.UI/wwwroot/js/chart.js
+++ Portal/KNU.RS.UI/wwwroot/js/chart.js
@@ -1,32 +1,26 @@
 window.chartExtensions = {
-	FILL_CHART : function()
-}
+	FILL_CHART: function (labels, data) {
+		var barChartData = {
+			labels: labels,
+			datasets: [{
+				label: 'Проведено',
+				backgroundColor: 'rgba(85, 206, 99, 0.5)',
+				borderColor: 'rgba(85, 206, 99, 1)',
+				borderWidth: 1,
+				data: data
+			}]
+		};
 
-$(document).ready(function (labels, data) {
-	
-	// Bar Chart
-
-	var barChartData = {
-		labels: labels,
-		datasets: [{
-			label: 'Проведено',
-			backgroundColor: 'rgba(85, 206, 99, 0.5)',
-			borderColor: 'rgba(85, 206, 99, 1)',
-			borderWidth: 1,
-			data: data
-		}]
-	};
-
-	var ctx = document.getElementById('bargraph').getContext('2d');
-	window.myBar = new Chart(ctx, {
-		type: 'bar',
-		data: barChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
+		var ctx = document.getElementById('bargraph').getContext('2d');
+		window.myBar = new Chart(ctx, {
+			type: 'bar',
+			data: barChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				}
 			}
-		}
-	});
-
-});
+		});
+    }
+}
--- Portal/KNU.RS.UI/wwwroot/js/sidebar.js
+++ Portal/KNU.RS.UI/wwwroot/js/sidebar.js
@@ -1,58 +1,26 @@
 ﻿window.blazorSidebarExtensions = {
-    SET_PANEL_ACTIVE: function setAuthNFailed() {
-        if (!($('#side-li-panel').hasClass('active'))) {
-            $('#side-li-panel').addClass('active');
-        }
-        if ($('#side-li-doctors').hasClass('active')) {
-            $('#side-li-doctors').removeClass('active');
-        }
-        if ($('#side-li-patients').hasClass('active')) {
-            $('#side-li-patients').removeClass('active');
-        }
-        if ($('#side-li-settings').hasClass('active')) {
-            $('#side-li-settings').removeClass('active');
-        }
+    SET_PANEL_ACTIVE: function() {
+        $('#side-li-panel').addClass('active');
+        $('#side-li-doctors').removeClass('active');
+        $('#side-li-patients').removeClass('active');
+        $('#side-li-settings').removeClass('active');
     },
-    SET_DOCTORS_ACTIVE: function setAuthNFailed() {
-        if ($('#side-li-panel').hasClass('active')) {
-            $('#side-li-panel').removeClass('active');
-        }
-        if (!($('#side-li-doctors').hasClass('active'))) {
-            $('#side-li-doctors').addClass('active');
-        }
-        if ($('#side-li-patients').hasClass('active')) {
-            $('#side-li-patients').removeClass('active');
-        }
-        if ($('#side-li-settings').hasClass('active')) {
-            $('#side-li-settings').removeClass('active');
-        }
+    SET_DOCTORS_ACTIVE: function() {
+        $('#side-li-panel').removeClass('active');
+        $('#side-li-doctors').addClass('active');
+        $('#side-li-patients').removeClass('active');
+        $('#side-li-settings').removeClass('active');
     },
-    SET_PATIENTS_ACTIVE: function setAuthNFailed() {
-        if ($('#side-li-panel').hasClass('active')) {
-            $('#side-li-panel').addClass('active');
-        }
-        if ($('#side-li-doctors').hasClass('active')) {
-            $('#side-li-doctors').removeClass('active');
-        }
-        if (!($('#side-li-patients').hasClass('active'))) {
-            $('#side-li-patients').addClass('active');
-        }
-        if ($('#side-li-settings').hasClass('active')) {
-            $('#side-li-settings').removeClass('active');
-        }
+    SET_PATIENTS_ACTIVE: function() {
+        $('#side-li-panel').removeClass('active');
+        $('#side-li-doctors').removeClass('active');
+        $('#side-li-patients').addClass('active');
+        $('#side-li-settings').removeClass('active');
     },
-    SET_SETTINGS_ACTIVE: function setAuthNFailed() {
-        if ($('#side-li-panel').hasClass('active')) {
-            $('#side-li-panel').removeClass('active');
-        }
-        if ($('#side-li-doctors').hasClass('active')) {
-            $('#side-li-doctors').removeClass('active');
-        }
-        if (!($('#side-li-patients').hasClass('active'))) {
-            $('#side-li-patients').addClass('active');
-        }
-        if ($('#side-li-settings').hasClass('active')) {
-            $('#side-li-settings').removeClass('active');
-        }
+    SET_SETTINGS_ACTIVE: function() {
+        $('#side-li-panel').removeClass('active');
+        $('#side-li-doctors').removeClass('active');
+        $('#side-li-patients').removeClass('active');
+        $('#side-li-settings').addClass('active');
     }
 }
