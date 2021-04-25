109650082f98d0bd96c53490ae20db1ba0974d9d
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-25 17:13:33 +0300

    patients table added

---
+++ Portal/KNU.RS.UI/Components/PatientsBase.cs
@@ -0,0 +1,29 @@
+﻿using KNU.RS.Logic.Models.Patient;
+using KNU.RS.Logic.Services.PatientService;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNU.RS.UI.Components
+{
+    public class PatientsBase : ComponentBase
+    {
+        [Inject]
+        protected IPatientService PatientService { get; set; }
+
+
+        protected List<PatientInfo> PatientsList { get; set; }
+
+        protected bool IsLoading { get; set; } = true;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true;
+            var patients = await PatientService.GetInfoAsync();
+            PatientsList = patients.ToList();
+            IsLoading = false;
+        }
+    }
+}
---
+++ Portal/KNU.RS.UI/Components/PatientsTableBase.cs
@@ -0,0 +1,32 @@
+﻿using KNU.RS.Logic.Configuration;
+using KNU.RS.Logic.Models.Patient;
+using KNU.RS.UI.Constants;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace KNU.RS.UI.Components
+{
+    public class PatientsTableBase : ComponentBase
+    {
+        [Inject]
+        protected IOptions<PhotoConfiguration> Options { get; set; }
+
+
+        [Parameter]
+        public List<PatientInfo> Patients { get; set; }
+
+        protected int Counter = 1;
+
+
+        protected string GetPhotoURI(PatientInfo patient)
+        {
+            if (patient.HasPhoto)
+            {
+                return $"{StaticFileConstants.PhotosRequestPath}/{patient.UserId}.{Options.Value.Extension}";
+            }
+
+            return "img/user.jpg";
+        }
+    }
+}
