bcd5d035170f5ac29885f62344e30e3234bf8808
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-25 21:10:00 +0300

    pagination added

--- Portal/KNU.RS.UI/Components/PatientsTableBase.cs
+++ Portal/KNU.RS.UI/Components/PatientsTableBase.cs
@@ -1,4 +1,5 @@
-﻿using KNU.RS.Logic.Configuration;
+﻿using KNU.RS.Logic.Collections;
+using KNU.RS.Logic.Configuration;
 using KNU.RS.Logic.Models.Patient;
 using KNU.RS.UI.Constants;
 using Microsoft.AspNetCore.Components;
@@ -16,8 +17,44 @@ namespace KNU.RS.UI.Components
         [Parameter]
         public List<PatientInfo> Patients { get; set; }
 
+        protected PaginatedList<PatientInfo> PatientsPage { get; set; }
+
+
         protected int Counter = 1;
 
+        private int PageSize = 10;
+        private int PagesForReference = 5;
+
+        protected List<int> AvailablePages { get; set; } = new List<int>();
+
+        protected override void OnParametersSet()
+        {
+            PatientsPage = PaginatedList<PatientInfo>.Create(Patients, 1, PageSize);
+            SetAvailablePages();
+        }
+
+        protected void SetAvailablePages()
+        {
+            var resultPages = new List<int>();
+
+            if (PatientsPage.HasPreviousPage)
+            {
+                resultPages.Add(PatientsPage.PageIndex - 1);
+            }
+
+            for (int i = PatientsPage.PageIndex; i <= PatientsPage.TotalPages && resultPages.Count < PagesForReference; ++i)
+            {
+                resultPages.Add(i);
+            }
+
+            AvailablePages = resultPages;
+        }
+
+        protected void GoToPage(int pageNumber)
+        {
+            PatientsPage = PaginatedList<PatientInfo>.Create(Patients, pageNumber, PageSize);
+            SetAvailablePages();
+        }
 
         protected string GetPhotoURI(PatientInfo patient)
         {
