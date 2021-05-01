0def90a1c9d22b436189a2705cc4553238c0aafb
Oleksii Bohusevych <olboh@smart-it.com>
2021-05-02 02:27:05 +0300

    high level role model added

--- Portal/KNU.RS.UI/Components/ViewDoctorBase.cs
+++ Portal/KNU.RS.UI/Components/ViewDoctorBase.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using KNU.RS.Logic.Models.Doctor;
+using KNU.RS.Logic.Services.DoctorService;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
 namespace KNU.RS.UI.Components
 {
     public class ViewDoctorBase : ComponentBase
     {
+        [Inject]
+        protected IDoctorService DoctorService { get; set; }
 
+        [Parameter]
+        public Guid Id { get; set; }
 
+        protected DoctorInfo Doctor { get; set; }
+
+        protected bool IsLoading { get; set; } = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true;
+            Doctor = await DoctorService.GetInfoAsync(Id);
+            IsLoading = false;
+        }
     }
 }
--- Portal/KNU.RS.UI/Components/ViewPatientBase.cs
+++ Portal/KNU.RS.UI/Components/ViewPatientBase.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using KNU.RS.Logic.Models.Patient;
+using KNU.RS.Logic.Services.PatientService;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
 namespace KNU.RS.UI.Components
 {
     public class ViewPatientBase : ComponentBase
     {
+        [Inject]
+        protected IPatientService PatientService { get; set; }
 
+        [Parameter]
+        public Guid Id { get; set; }
+
+        protected PatientInfo Patient { get; set; } = new PatientInfo();
+
+        protected bool IsLoading { get; set; } = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true;
+            Patient = await PatientService.GetInfoAsync(Id);
+            IsLoading = false;
+        }
     }
 }
